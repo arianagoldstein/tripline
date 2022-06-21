16e7201af7842e35581707d3926f0857fe670cdd
Ariana Goldstein <arianagoldstein@fb.com>
2022-06-21 14:05:53 -0700

    added Trip class with getters and setters for fields in Parse

---
+++ app/src/main/java/com/example/tripline/models/Trip.java
@@ -0,0 +1,82 @@
+package com.example.tripline.models;
+
+import com.parse.ParseClassName;
+import com.parse.ParseFile;
+import com.parse.ParseGeoPoint;
+import com.parse.ParseObject;
+
+import java.util.Date;
+
+// a Trip object represents a trip created by a user that will be displayed on their profile and in the stream
+@ParseClassName("Trip")
+public class Trip extends ParseObject {
+
+    public static final String KEY_TITLE = "title";
+    public static final String KEY_DESCRIPTION = "description";
+    public static final String KEY_LOCATION = "location";
+    public static final String KEY_START_DATE = "startDate";
+    public static final String KEY_END_DATE = "endDate";
+    public static final String KEY_COVER_PHOTO = "coverPhoto";
+    public static final String KEY_AUTHOR = "author";
+
+    public Trip() {
+
+    }
+
+    public String getTitle() {
+        return getString(KEY_TITLE);
+    }
+
+    public void setTitle(String title) {
+        put(KEY_TITLE, title);
+    }
+
+    public String getDescription() {
+        return getString(KEY_DESCRIPTION);
+    }
+
+    public void setDescription(String description) {
+        put(KEY_DESCRIPTION, description);
+    }
+
+    public ParseGeoPoint getLocation() {
+        return getParseGeoPoint(KEY_LOCATION);
+    }
+
+    public void setLocation(ParseGeoPoint location) {
+        put(KEY_LOCATION, location);
+    }
+
+    public Date getStartDate() {
+        return getDate(KEY_START_DATE);
+    }
+
+    public void setStartDate(Date startDate) {
+        put(KEY_START_DATE, startDate);
+    }
+
+    public Date getEndDate() {
+        return getDate(KEY_END_DATE);
+    }
+
+    public void setEndDate(Date endDate) {
+        put(KEY_END_DATE, endDate);
+    }
+
+    public ParseFile getCoverPhoto() {
+        return getParseFile(KEY_COVER_PHOTO);
+    }
+
+    public void setCoverPhoto(ParseFile coverPhoto) {
+        put(KEY_COVER_PHOTO, coverPhoto);
+    }
+
+    public User getAuthor() {
+        return (User) getParseUser(KEY_AUTHOR);
+    }
+
+    public void setAuthor(User user) {
+        put(KEY_AUTHOR, user);
+    }
+
+}
