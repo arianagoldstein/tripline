513ead721f2de78a68ff14ea39031bdf02937bd8
Ariana Goldstein <arianagoldstein@fb.com>
2022-06-22 14:18:16 -0700

    implementation for RecyclerView of Trips in stream
    created Trip XML layout, implemented TripAdapter, and set the RecyclerView adapter

--- app/src/main/java/com/example/tripline/TripAdapter.java
+++ app/src/main/java/com/example/tripline/TripAdapter.java
@@ -5,10 +5,13 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.tripline.models.Trip;
 
 import java.util.List;
@@ -43,14 +46,39 @@ public class TripAdapter extends RecyclerView.Adapter<TripAdapter.ViewHolder> {
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
+        // declaring XML elements
+        private final TextView tvTripTitle;
+        private final TextView tvLocation;
+        private final TextView tvStartDate;
+        private final TextView tvEndDate;
+        private final ImageView ivCoverPhoto;
+        private final TextView tvDescription;
+        private final TextView tvAuthorName;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            // locating the XML elements in the layout
+            tvTripTitle = itemView.findViewById(R.id.tvTripTitleStream);
+            tvLocation = itemView.findViewById(R.id.tvLocationStream);
+            tvStartDate = itemView.findViewById(R.id.tvStartDateStream);
+            tvEndDate = itemView.findViewById(R.id.tvEndDateStream);
+            ivCoverPhoto = itemView.findViewById(R.id.ivCoverPhotoStream);
+            tvDescription = itemView.findViewById(R.id.tvDescriptionStream);
+            tvAuthorName = itemView.findViewById(R.id.tvAuthorNameStream);
 
             itemView.setOnClickListener(this);
         }
 
         public void bind(Trip trip) {
-
+            // populating the XML elements with the details of this trip
+            tvTripTitle.setText(trip.getTitle());
+            tvLocation.setText(trip.getLocation().toString());
+            tvStartDate.setText(trip.getStartDate().toString());
+            tvEndDate.setText(trip.getEndDate().toString());
+            Glide.with(context).load(trip.getCoverPhoto().getUrl()).into(ivCoverPhoto);
+            tvDescription.setText(trip.getDescription());
+            tvAuthorName.setText(trip.getAuthor().getFirstName() + " " + trip.getAuthor().getLastName());
         }
 
         @Override
--- app/src/main/java/com/example/tripline/ui/stream/StreamFragment.java
+++ app/src/main/java/com/example/tripline/ui/stream/StreamFragment.java
@@ -7,15 +7,30 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.tripline.TripAdapter;
 import com.example.tripline.databinding.FragmentStreamBinding;
+import com.example.tripline.models.Trip;
+
+import java.util.ArrayList;
+import java.util.List;
 
 // this fragment will display a stream of trips created by the logged-in user and other users
 public class StreamFragment extends Fragment {
 
     private FragmentStreamBinding binding;
+    private RecyclerView rvTrips;
+    protected TripAdapter adapter;
+    protected List<Trip> allTrips;
+
+    public StreamFragment() {
+
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -26,6 +41,20 @@ public class StreamFragment extends Fragment {
         View root = binding.getRoot();
 
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // rvTrips = view.findViewById(R.id.rvTrips);
+
+        allTrips = new ArrayList<>();
+        adapter = new TripAdapter(getContext(), allTrips);
+        binding.rvTrips.setAdapter(adapter);
+
+        LinearLayoutManager llm = new LinearLayoutManager(getContext());
+        rvTrips.setLayoutManager(llm);
     }
 
     @Override
