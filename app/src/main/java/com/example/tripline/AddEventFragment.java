30c60a7c2cbb1285d7267b2c198657616a1f1b3f
Ariana Goldstein <arianagoldstein@fb.com>
2022-06-29 11:38:02 -0700

    user can upload multiple photos to events with preview in a gridview

--- app/src/main/java/com/example/tripline/AddEventFragment.java
+++ app/src/main/java/com/example/tripline/AddEventFragment.java
@@ -1,6 +1,14 @@
 package com.example.tripline;
 
+import android.content.ClipData;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,7 +27,18 @@ import com.example.tripline.databinding.FragmentAddEventBinding;
 import com.example.tripline.models.Event;
 import com.example.tripline.models.Trip;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.SaveCallback;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.AbstractCollection;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddEventFragment extends Fragment implements AdapterView.OnItemSelectedListener {
 
@@ -28,6 +47,14 @@ public class AddEventFragment extends Fragment implements AdapterView.OnItemSele
     private String activityType;
     private Trip trip;
     private Boolean nothingSelected;
+
+    // variables for multiple photo upload
+    private static final int PICK_IMAGE_MULTIPLE = 1;
+    private static final int RESULT_OK = -1;
+    String imageEncoded;
+    List<String> imagesEncodedList;
+    private GalleryAdapter galleryAdapter;
+    private List<Uri> imageUriList;
 
     public AddEventFragment() {
         // Required empty public constructor
@@ -61,8 +88,26 @@ public class AddEventFragment extends Fragment implements AdapterView.OnItemSele
         binding.spActivityType.setOnItemSelectedListener(this);
 
         // keeping track of which trip this event is associated with
-        // tripId = getArguments().getString("tripId");
         trip = ((MainActivity) getContext()).selectedTrip;
+
+        // initializing list of images
+        imageUriList = new ArrayList<>();
+
+        // onclick listener for uploading multiple images
+        binding.btnAddPhotos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // clearing the list so we don't add photos twice
+                imageUriList.clear();
+
+                // creating a new intent to select images from gallery
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,"Select Picture"), PICK_IMAGE_MULTIPLE);
+            }
+        });
 
         // outlining onclick listener for add event button
         binding.btnAddEvent.setOnClickListener(new View.OnClickListener() {
@@ -71,34 +116,90 @@ public class AddEventFragment extends Fragment implements AdapterView.OnItemSele
                 String title = binding.etTitleEvent.getText().toString();
                 String description = binding.etDescriptionEvent.getText().toString();
 
+                // user must select an activity type
                 if (nothingSelected || activityType.equals("select an activity type")) {
                     Toast.makeText(getContext(), "Activity type cannot be empty!", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
+                // user must include a title
                 if (title.isEmpty()) {
                     Toast.makeText(getContext(), "Title cannot be empty!", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
+                // user must include a description
                 if (description.isEmpty()) {
                     Toast.makeText(getContext(), "Description cannot be empty!", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
+                // constructing array that will store user photos
+                JSONArray photoArray = new JSONArray();
+
+                // populating array with images (ParseFiles)
+                for (int i = 0; i < imageUriList.size(); i++) {
+                    // getting the URI at this index
+                    Uri uri = imageUriList.get(i);
+                    Log.i(TAG, "image URI: " + uri);
+
+                    Bitmap image = loadFromUri(uri);
+
+                    // compressing the image so that it can upload to Parse successfully
+                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                    image.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                    ParseFile file = new ParseFile("img" + i + ".jpg", stream.toByteArray());
+
+                    // constructing a new JSONObject to add to the array to save to Parse
+                    JSONObject jsonObject = new JSONObject();
+                    try {
+                        jsonObject.put("image", file);
+                        photoArray.put(jsonObject);
+                    } catch (JSONException e) {
+                        Log.e(TAG, "Error adding JSON object.");
+                    }
+                }
+
+                // user must upload at least one image
+                if (photoArray.length() == 0) {
+                    Toast.makeText(getContext(), "You must upload at least one image!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
                 // calling function to post this new event to the database
-                postEvent(title, description, activityType, trip);
+                postEvent(title, description, activityType, trip, photoArray);
             }
         });
+
+    }
+
+    // function that creates a Bitmap from the specified URI
+    private Bitmap loadFromUri(Uri photoUri) {
+        Bitmap image = null;
+        try {
+            // check version of Android on device
+            if(Build.VERSION.SDK_INT > 27){
+                // on newer versions of Android, use the new decodeBitmap method
+                ImageDecoder.Source source = ImageDecoder.createSource(getContext().getContentResolver(), photoUri);
+                image = ImageDecoder.decodeBitmap(source);
+            } else {
+                // support older versions of Android by using getBitmap
+                image = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), photoUri);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return image;
     }
 
     // saves this new event to the Parse database
-    private void postEvent(String title, String description, String activityType, Trip trip) {
+    private void postEvent(String title, String description, String activityType, Trip trip, JSONArray photoArray) {
         Event event = new Event();
         event.setTitle(title);
         event.setDescription(description);
         event.setActivityType(activityType);
         event.setTrip(trip);
+        event.setPhotos(photoArray);
 
         event.saveInBackground(new SaveCallback() {
             @Override
@@ -134,5 +235,82 @@ public class AddEventFragment extends Fragment implements AdapterView.OnItemSele
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    // function that gets the images selected by the user and puts them into a gridview
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        try {
+            // when an image is picked
+            if (requestCode == PICK_IMAGE_MULTIPLE && resultCode == RESULT_OK && null != data) {
+                // get the Image from data
+                String[] filePathColumn = { MediaStore.Images.Media.DATA };
+                imagesEncodedList = new ArrayList<String>();
+                if(data.getData()!=null){
+
+                    Uri mImageUri=data.getData();
+
+                    // get the cursor
+                    Cursor cursor = getContext().getContentResolver().query(mImageUri,
+                            filePathColumn, null, null, null);
+                    // move to first row
+                    cursor.moveToFirst();
+
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    imageEncoded  = cursor.getString(columnIndex);
+                    cursor.close();
+
+                    ArrayList<Uri> mArrayUri = new ArrayList<Uri>();
+                    mArrayUri.add(mImageUri);
+                    galleryAdapter = new GalleryAdapter(getContext().getApplicationContext(),mArrayUri);
+                    binding.gvGallery.setAdapter(galleryAdapter);
+                    binding.gvGallery.setVerticalSpacing(binding.gvGallery.getHorizontalSpacing());
+                    ViewGroup.MarginLayoutParams mlp = (ViewGroup.MarginLayoutParams) binding.gvGallery
+                            .getLayoutParams();
+                    mlp.setMargins(0, binding.gvGallery.getHorizontalSpacing(), 0, 0);
+
+                } else {
+                    if (data.getClipData() != null) {
+                        ClipData mClipData = data.getClipData();
+                        ArrayList<Uri> mArrayUri = new ArrayList<Uri>();
+                        for (int i = 0; i < mClipData.getItemCount(); i++) {
+
+                            ClipData.Item item = mClipData.getItemAt(i);
+                            Uri uri = item.getUri();
+                            mArrayUri.add(uri);
+
+                            // storing URIs in a list to later convert to ParseFiles
+                            imageUriList.add(uri);
+
+                            // get the cursor
+                            Cursor cursor = getContext().getContentResolver().query(uri, filePathColumn, null, null, null);
+                            // move to first row
+                            cursor.moveToFirst();
+
+                            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                            imageEncoded  = cursor.getString(columnIndex);
+                            imagesEncodedList.add(imageEncoded);
+                            cursor.close();
+
+                            galleryAdapter = new GalleryAdapter(getContext().getApplicationContext(),mArrayUri);
+                            binding.gvGallery.setAdapter(galleryAdapter);
+                            binding.gvGallery.setVerticalSpacing(binding.gvGallery.getHorizontalSpacing());
+                            ViewGroup.MarginLayoutParams mlp = (ViewGroup.MarginLayoutParams) binding.gvGallery
+                                    .getLayoutParams();
+                            mlp.setMargins(0, binding.gvGallery.getHorizontalSpacing(), 0, 0);
+
+                        }
+                        Log.v(TAG, "Selected Images" + mArrayUri.size());
+                    }
+                }
+            } else {
+                Toast.makeText(getContext(), "You haven't picked an image", Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(getContext(), "Something went wrong", Toast.LENGTH_LONG)
+                    .show();
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
